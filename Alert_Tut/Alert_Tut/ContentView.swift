7dea00176c0de49b8440a04d57b74f8601943814
Tyrel Jackson <tyreljackson904@gmail.com>
2020-11-06 21:37:57 -0700

    methods

--- Alert_Tut/Alert_Tut/ContentView.swift
+++ Alert_Tut/Alert_Tut/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
     @State var sliderValue: Double = 0.0
-    var targetNumber: Int = 100
+    @State var targetNumber: Int = Int.random(in: 1...100)
     
     
     var body: some View {
@@ -24,16 +24,24 @@ struct ContentView: View {
                 Text("Get the slider as close as you can to: \(targetNumber)")
             }
             Spacer()
-            //Text("\(sliderValue)")
             // 2. Slider
             HStack {
                 Text("1")
                 Slider(value: self.$sliderValue, in: 1...100, step: 1)
                 Text("100")
             }.padding()
-// 3.
-            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            // 3.
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
                 Text("Hit Me")
+                
+            }.alert(isPresented: $alertIsVisible) { () ->
+                Alert in
+                return Alert(title: Text("Hey There!"), message: Text(
+                    "The slider's value is:  \(Int(sliderValue)).\n" +
+                    "You scored \(self.pointsForCurrentRound())"
+                ), dismissButton: .default(Text("Play Again")))
             }
             Spacer()
             HStack(spacing: 30) {
@@ -46,17 +54,24 @@ struct ContentView: View {
                     Text("Info")
                 }
             }
-//            Button(action: {
-//                self.alertIsVisible = true
-//            }) {
-//                Text("Knock Knock")
-//            }.alert(isPresented: $alertIsVisible) { () ->
-//                Alert in
-//                return Alert(title: Text("Whos There?"), message: Text("your mother"), dismissButton: .default(Text("haha")))
-//            }
         }
     }
+    
+    func pointsForCurrentRound() -> Int {
+        
+        var roundedValue: Int = Int(self.sliderValue)
+        var difference: Int = self.targetNumber - roundedValue
+        
+        if difference < 0 {
+            difference *= -1
+        }
+        var awarededPoints: Int = 100 - difference
+        
+        return awarededPoints
+    }
 }
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
