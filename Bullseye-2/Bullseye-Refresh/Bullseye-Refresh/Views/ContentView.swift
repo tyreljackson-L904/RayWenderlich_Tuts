2b557c950a4ef37143ad5f59c1507846ef443ca6
Tyrel Jackson <tyreljackson904@gmail.com>
2021-01-04 13:52:40 -0700

    view modifiers

--- Bullseye-2/Bullseye-Refresh/Bullseye-Refresh/Views/ContentView.swift
+++ Bullseye-2/Bullseye-Refresh/Bullseye-Refresh/Views/ContentView.swift
@@ -16,38 +16,50 @@ struct ContentView: View {
     @State private var game: Game = Game()
     
     var body: some View {
-        VStack {
-            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-            Text(String(game.target))
-                .fontWeight(.black)
-                .font(.system(size: 28))
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
                     .fontWeight(.bold)
-                Slider(value: $sliderValue, in: 1...100)
-                Text("100")
-                    .fontWeight(.bold)
-            }
-            Button(action: {
-                // do something
-                showAlert.toggle()
-            }, label: {
-                Text("HIT ME")
-                    .fontWeight(.bold)
-            })
-            .alert(isPresented: $showAlert) {
-                let roundedValue: Int = Int(sliderValue.rounded())
+                    .multilineTextAlignment(.center)
+                Text(String(game.target))
+                    .fontWeight(.black)
+                    .font(.system(size: 28))
+                HStack {
+                    Text("1")
+                        .fontWeight(.bold)
+                    Slider(value: $sliderValue, in: 1...100)
+                    Text("100")
+                        .fontWeight(.bold)
+                }
+                Button(action: {
+                    // do something
+                    showAlert.toggle()
+                }, label: {
+                    Text("HIT ME")
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .frame(width: 125, height: 65)
+                        .background(
+                            ZStack {
+                                Color("ButtonColor")
+                                LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                        })
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                    
+                })
+                .alert(isPresented: $showAlert) {
+                    let roundedValue: Int = Int(sliderValue.rounded())
+                    
+                    return Alert(title: Text("Nice One"), message: Text("The slide value is  \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"))
+                }
                 
-                return Alert(title: Text("Nice One"), message: Text("The slide value is  \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"))
             }
-                .frame(width: 125, height: 65)
-                .background(Color.blue)
-                .cornerRadius(20)
-                .foregroundColor(.white)
-            
-        }.padding()
+            .padding()
+            .padding(.vertical)
+        }
+        
     }
 }
 
