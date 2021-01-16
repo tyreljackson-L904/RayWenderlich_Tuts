ba58e463050f6f21de521b2b3d09333fe887a610
Tyrel Jackson <tyreljackson904@gmail.com>
2021-01-16 15:11:48 -0700

    init commit

---
+++ ReadMe/ReadMe/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by Tyrel Jackson on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "book")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .font(Font.title.weight(.light))
+                .foregroundColor(.secondary)
+            Text("Title")
+                .font(.title2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMe/ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Tyrel Jackson on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
